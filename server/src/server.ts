192969c66272defe22f080c6363c3a2eb8eea2d5
Janderson Pizzially Vilas Boas <jandersonpizziallyvilasboas@gmail.com>
2020-08-05 12:41:04 -0300

    chore: EditorConfig

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,4 +8,6 @@ app.get('/users', (request, response) => {
   return response.json('Hello World');
 })
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log('✔ Server started on post 3333!')
+});
