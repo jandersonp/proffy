2c9f8f12b103eb0e6c638a0e66ae2ca27e0fd24d
Janderson Pizzially Vilas Boas <jandersonpizziallyvilasboas@gmail.com>
2020-08-05 13:02:35 -0300

    chore: ESlint

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,10 +4,8 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/users', (request, response) => {
-  return response.json('Hello World');
-})
+app.get('/users', (request, response) => response.json('Hello World'));
 
 app.listen(3333, () => {
-  console.log('✔ Server started on post 3333!')
+  console.log('✔ Server started on post 3333!');
 });
